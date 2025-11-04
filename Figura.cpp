f5264b55548118a8f8d0a953d1af2cdbf05eebc0
CECILIA ROMERO GRANADOS <cromero52207@ucq.edu.mx>
2025-11-04 14:18:07 -0600

    Revise description comment in Figura.cpp
    Updated the description comment for clarity.

--- Figura.cpp
+++ Figura.cpp
@@ -3,5 +3,6 @@
 // Profesor: M. en C. Adrián Ulises González Casillas
 // Práctica: Entrega del Parcial – Ejercicios Listas, Doblemente ligadas, Stacks y Queues
 // Parte: 4 – Figura.cpp
-// Descripción: Este archivo no tiene código, solo está aquí para mantener completa la estructura del proyecto.
+// Descripción: Este archivo no contiene código porque toda la lógica está en "Figura.h".
+// Se incluye únicamente para mantener la estructura completa y organizada del proyecto, tal como se pidió.
 // Alumnos: Cecilia Romero Granados, Alexis Jeshua Arrona
